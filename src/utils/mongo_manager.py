9a61771fb458bbd87abe18b8332031181a861cbe
Marilyth <baermay98@gmail.com>
2024-01-28 10:49:04 +0100

    Revert test database

--- src/utils/mongo_manager.py
+++ src/utils/mongo_manager.py
@@ -67,7 +67,7 @@ class ItemPricesCollection:
 
 
 class MongoManager:
-    def __init__(self, connection_string: str, database_name: str = "TibiaMarketTracker_Dev"):
+    def __init__(self, connection_string: str, database_name: str = "TibiaMarketTracker"):
         self.client = pymongo.MongoClient(connection_string)
         self.database = self.client[database_name]
         self.item_prices = self.database["ItemPrices"]
