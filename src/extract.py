383a511dfb92830a3a23ea1137271d701a642637
May <baermay98@gmail.com>
2025-10-12 13:19:18 +0200

    Add some output

--- src/extract.py
+++ src/extract.py
@@ -220,6 +220,8 @@ def reorder_schedule():
         slot_length = len(schedule.hours[str(current_slot)])
         slot_capacity = (current_slot // slots_per_bucket) + 1
 
+        print(f"{character.server} updates at {current_slot}AM German time, every {slot_capacity} days.")
+
         if slot_length >= slot_capacity:
             current_slot += 1
 
